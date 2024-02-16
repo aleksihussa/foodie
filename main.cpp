899c5810a125ab08905e5fb8dc01885213475988
aleksihussa <aleksi.hussa@gmail.com>
2024-02-16 17:34:01 +0200

    cleanup

--- main.cpp
+++ main.cpp
@@ -4,15 +4,8 @@
 using namespace std;
 
 int main() {
-  // LunchMenuChecker checker;
-  // checker.fetchMenu();
-  std::string url = "https://api.example.com/data";
   string test = "https://unisafka.fi/static/json/2024/7/v.json";
-  try {
-    json jsonResponse = HTTPClient::getJson(test);
-    std::cout << jsonResponse.dump(4) << std::endl; // Pretty print the JSON
-  } catch (const json::exception &e) {
-    std::cerr << "Exception: " << e.what() << std::endl;
-  }
+  json jsonResponse = HTTPClient::getJson(test);
+  std::cout << jsonResponse.dump(4) << std::endl;
   return 0;
 }
